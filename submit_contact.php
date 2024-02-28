c80c31391c24912e1095239dbdcb8e4472cafbad
ekram9 <whoiam896@gmail.com>
2024-02-28 21:19:04 +0600

    first commit

---
+++ submit_contact.php
@@ -0,0 +1,36 @@
+<!-- <
+// if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get the form fields
+    // $name = $_POST['name'];
+    // $email = $_POST['email'];
+    // $phone = $_POST['phone'];
+    // $message = $_POST['message'];
+
+    // Email address where you want to receive the form submissions
+    // $to_email = "whoiam896@.com"; // Replace with your email address
+
+    // // Email subject
+    // $subject = "New Contact Form Submission";
+
+    // Email message
+    // $email_message = "Name: $name\n";
+    // $email_message .= "Email: $email\n";
+    // $email_message .= "Phone: $phone\n\n";
+    // $email_message .= "Message:\n$message";
+
+    // Headers
+    // $headers = "From: $name <$email>";
+
+    // Send email
+//     if (mail($to_email, $subject, $email_message, $headers)) {
+//         // Email sent successfully
+//         echo "Thank you for contacting us. We will get back to you soon.";
+//     } else {
+//         // Email sending failed
+//         echo "Sorry, something went wrong. Please try again later.";
+//     }
+// } else {
+//     // If it's not a POST request, redirect to the contact page
+//     header("Location: contact.html");
+// }
+// -->
